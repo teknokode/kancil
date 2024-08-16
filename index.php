31aa3e8571048fa2b44f9acd7036daf87f166070
Pino <apenta.hosting@gmail.com>
2024-08-17 00:06:17 +0700

    Mode pada .env

--- index.php
+++ index.php
@@ -10,9 +10,27 @@ require_once ("./vendor/autoload.php");
 
 use Kancil\Core\Router;
 
+// Inisialisai umum, silakan disesuaikan
+set_time_limit(60 * 10);
+date_default_timezone_set('Asia/Jakarta');
+
 // Muat file .env
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeload();
+
+// Cek mode dan set error report
+if ($_ENV["MODE"]=="development")
+{
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+
+} elseif ($_ENV["MODE"]=="production") 
+{
+    error_reporting(0 & ~E_WARNING & ~E_STRICT & ~E_NOTICE );
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
+}
 
 $_ENV["BASE_URL"] = rtrim( $_ENV["BASE_URL"],"/");        
 $_ENV["APP_PATH"] = __dir__;
