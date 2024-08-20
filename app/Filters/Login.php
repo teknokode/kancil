6066335b8af1b10616056f57cbe7904d477e8f7b
Pino <apenta.hosting@gmail.com>
2024-08-20 14:05:09 +0700

    Home

--- app/Filters/Login.php
+++ app/Filters/Login.php
@@ -6,17 +6,17 @@ class Login
 {
     public function index()
     {
-        redirect("skema/18", ["username"=>"aku"]);
+        //redirect("skema/18", ["username"=>"aku"]);
         // $encrypted_txt = encrypt("Ada apa dengan kamu?");
         // echo "Encrypted Text = " .$encrypted_txt. "\n";
         // $decrypted_txt = decrypt($encrypted_txt);
         // echo "Decrypted Text =" .$decrypted_txt. "\n";
 
-        if (!isset($_SESSION["is_login"]))
-        {
-            echo "Anda belum login";
-            return false;
-        }
+        // if (!isset($_SESSION["is_login"]))
+        // {
+        //     echo "Anda belum login";
+        //     return false;
+        // }
         return true;
     }
 }
